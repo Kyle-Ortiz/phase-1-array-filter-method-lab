b84927228100ece3930f1392ba1ebe76644598a5
Kyle Ortiz <kyleaortiz@gmail.com>
2021-07-03 22:16:11 -0500

    commit tree 5571a10fc77e0196160745b6b09be9c2576203ab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(driveArray, string) {
+  const matches = driveArray.filter((name) => {
+    if (name == string.toLowerCase()) {
+      return name;
+    } else {
+      return name == string;
+    }
+  });
+  return matches;
+}
+function fuzzyMatch(driveArray, string) {
+  const startMatches = driveArray.filter((name) => {
+    return name.startsWith(string);
+  });
+  return startMatches;
+}
+function matchName(driveObject, string) {
+  const matchObject = driveObject.filter((name, index) => {
+    return driveObject[index]["name"] == string;
+  });
+  return matchObject;
+}
